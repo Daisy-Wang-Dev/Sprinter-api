0e45050030cbcb7a51cb47905beb319fa9e7fbdc
Xueying Wang <daiszws19@gmail.com>
2023-06-01 11:58:15 +0100

    Add project data file

---
+++ routes/project.js
@@ -0,0 +1,18 @@
+const express =  require("express");
+const router = express.Router();
+const projectData = require("../data/projectData.json");
+const crypto = require("crypto");
+
+// Get a project
+router.get("/:id", (req, res)=>{
+    
+})
+
+// Post a new project
+router.post("/", (req,res)=>{
+  
+})
+
+
+
+module.exports =  router;
